94e0f06a4664f32c9ab351ba21d9f2f88e18a2d6
Ryan Hughes <ryanhughes@MacBook-Pro.local>
2020-10-29 13:57:33 +0000

    added ability for instructors to register

--- Project/app/src/main/java/com/personal/drivebooster/HomeFragment.java
+++ Project/app/src/main/java/com/personal/drivebooster/HomeFragment.java
@@ -5,21 +5,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-public class HomeFragment extends Fragment {
+import java.util.ArrayList;
+
+public class HomeFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     View view;
+    FirebaseAuth auth;
     Button logoutButton;
+    DatabaseReference databaseRef, dbUserRef;
+    Spinner instructorChoiceSpinner;
+    String instructorName;
+    ArrayList<String> instructorArray = new ArrayList<String>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         view = inflater.inflate(R.layout.home_fragment, container, false);
         logoutButton = view.findViewById(R.id.logout_button);
+        getInstructors();
+        instructorChoiceSpinner = view.findViewById(R.id.choose_instructor_spinner);
+
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_spinner_item, instructorArray);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        instructorChoiceSpinner.setAdapter(spinnerArrayAdapter);
+        instructorChoiceSpinner.setOnItemSelectedListener(this);
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +58,56 @@ public class HomeFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    public void getInstructors(){
+        databaseRef = FirebaseDatabase.getInstance().getReference().child("Instructors");
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds: snapshot.getChildren()){
+                    String uId = (String) ds.getKey();
+                    String name = ds.child("name").getValue(String.class);
+                    instructorArray.add(name);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        instructorName = item;
+        Toast.makeText(getContext(), "Instructor chosen", Toast.LENGTH_SHORT).show();
+        dbUserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        Users userObj = new Users(instructorName);
+        FirebaseUser firebaseUser = auth.getCurrentUser();
+
+        dbUserRef.child(firebaseUser.getUid()).setValue(userObj)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getContext(), "Instructor chosen", Toast.LENGTH_SHORT).show();
+
+                            instructorChoiceSpinner.setVisibility(View.GONE);
+                        }
+                        else{
+                            Toast.makeText(getContext(), "Could not choose instructor", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
     }
 
 }
--- Project/app/src/main/java/com/personal/drivebooster/RegisterActivity.java
+++ Project/app/src/main/java/com/personal/drivebooster/RegisterActivity.java
@@ -7,6 +7,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -19,18 +21,23 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     EditText editTextUsername, editTextEmail, editTextPassword, editTextPasswordCnf;
-    Button registerButton;
-    Button buttonLogin;
     FirebaseAuth auth;
-    DatabaseReference databaseReference;
-    Spinner userSpinner;
+    DatabaseReference databaseUsersReference, databaseInstructorsReference, databaseRef, dbUserRef;
     String userType;
+    Spinner userSpinner;
+    ArrayList<String> instructorArray = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +49,7 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
         editTextPassword = findViewById(R.id.edit_text_password);
         editTextPasswordCnf = findViewById(R.id.edit_text_passwordCnf);
         auth = FirebaseAuth.getInstance();
+
         userSpinner = findViewById(R.id.user_type_spinner);
 
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.user_type_array, android.R.layout.simple_spinner_item);
@@ -72,24 +80,11 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
                             if(task.isSuccessful()){
                                 startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                                 Toast.makeText(getApplicationContext(), "User created", Toast.LENGTH_SHORT).show();
-
-                                databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
-
-                                Users user_obj = new Users(editTextUsername.getText().toString(), editTextEmail.getText().toString(), editTextPassword.getText().toString(), userType);
-                                FirebaseUser firebaseUser = auth.getCurrentUser();
-
-                                databaseReference.child(firebaseUser.getUid()).setValue(user_obj)
-                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                if (task.isSuccessful()){
-                                                    Toast.makeText(getApplicationContext(), "User details stored", Toast.LENGTH_SHORT).show();
-                                                }
-                                                else{
-                                                    Toast.makeText(getApplicationContext(), "User data could not be saved", Toast.LENGTH_SHORT).show();
-                                                }
-                                            }
-                                        });
+                                if(userType.equals("Pupil")){
+                                    registerPupil(name, email, password);
+                                } else{
+                                    registerInstructor(name, email, password);
+                                }
 
                             }else{
                                 Toast.makeText(getApplicationContext(), "User could not be created at this time", Toast.LENGTH_SHORT).show();
@@ -99,15 +94,67 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
         }
     }
 
+    public void registerPupil(String name, String email, String password){
+
+        name = editTextUsername.getText().toString();
+        email = editTextEmail.getText().toString();
+        password = editTextPassword.getText().toString();
+
+        databaseUsersReference = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        Users user_obj = new Users(name, email, password, userType);
+        FirebaseUser firebaseUser = auth.getCurrentUser();
+
+        databaseUsersReference.child(firebaseUser.getUid()).setValue(user_obj)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "User details stored", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "User data could not be saved", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void registerInstructor(String name, String email, String password){
+        name = editTextUsername.getText().toString();
+        email = editTextEmail.getText().toString();
+        password = editTextPassword.getText().toString();
+
+
+        databaseInstructorsReference = FirebaseDatabase.getInstance().getReference().child("Instructors");
+
+        Instructors instructor_obj = new Instructors(name, email, password, userType);
+        FirebaseUser fbUser = auth.getCurrentUser();
+
+        databaseInstructorsReference.child(fbUser.getUid()).setValue(instructor_obj)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "User details stored", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "User data could not be saved", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String item = parent.getItemAtPosition(position).toString();
         userType = item;
     }
 
+
+
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-
     }
 }
 
