554b0d3b563bf62e97b328e998e717e2fba929ef
Ryan Hughes <ryanhughes@MacBook-Pro.local>
2020-11-29 13:26:43 +0000

    different main activity for instructors

---
+++ Project/app/src/main/java/com/personal/drivebooster/InstructorMainActivity.kt
@@ -0,0 +1,11 @@
+package com.personal.drivebooster
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InstructorMainActivity :  AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instructor_main)
+    }
+}
