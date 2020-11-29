6fdd01037d8509dc0a6d0032dc51b444301a969a
Ryan Hughes <ryanhughes@MacBook-Pro.local>
2020-11-29 14:16:41 +0000

    different activities show for different user type

--- Project/app/src/main/java/com/personal/drivebooster/InstructorMainActivity.kt
+++ Project/app/src/main/java/com/personal/drivebooster/InstructorMainActivity.kt
@@ -1,11 +1,25 @@
 package com.personal.drivebooster
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.internal.ContextUtils.getActivity
+import com.google.firebase.auth.FirebaseAuth
+
 
 class InstructorMainActivity :  AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_instructor_main)
+        var logoutButton = findViewById<Button>(R.id.instructor_logout_button)
+
+        logoutButton.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+
+            startActivity(Intent(this, LoginActivity::class.java))
+            this.finish()
+        }
+
     }
 }
