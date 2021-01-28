bffbe6060250bc9a4cb38b0378b6a62c5080ea86
Ryan Hughes <ryanhughes@MacBook-Pro.local>
2021-01-28 12:37:16 +0000

    back button removed from nav fragments

--- Project/app/src/main/java/com/personal/drivebooster/MainActivity.kt
+++ Project/app/src/main/java/com/personal/drivebooster/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,10 +25,14 @@ class MainActivity : AppCompatActivity() {
     //method to determine what the back button does on this screen
     override fun onSupportNavigateUp() = findNavController(R.id.main_navigation_fragment).navigateUp()
 
+    val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.homeFragment2, R.id.navigation_booking,
+            R.id.navigation_my_details))
+
     //method to set up the bottom navigation
     private fun setUpNavigation(){
         val navController = Navigation.findNavController(this, R.id.main_navigation_fragment)
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         NavigationUI.setupWithNavController(bottom_tab_navigation, navController)
     }
 }
