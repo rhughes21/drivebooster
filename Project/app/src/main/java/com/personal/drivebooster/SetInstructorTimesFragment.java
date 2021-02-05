7d1d1e74e14ef94c73349643fbb8a0fa81dc7978
Ryan Hughes <ryanhughes@MacBook-Pro.local>
2021-02-05 16:09:02 +0000

    instructor now sets the times for everyday of the week. Times stored to database

--- Project/app/src/main/java/com/personal/drivebooster/SetInstructorTimesFragment.java
+++ Project/app/src/main/java/com/personal/drivebooster/SetInstructorTimesFragment.java
@@ -7,25 +7,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SetInstructorTimesFragment extends Fragment{
+public class SetInstructorTimesFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     View view;
+    FirebaseAuth auth;
     TimePicker timePicker;
-    Button addTime;
-    String lastTimeChosen;
+    Button addTime, submitTimes;
+    String lastTimeChosen, dayType;
+    Spinner daySpinner;
+    DatabaseReference databaseTimesReference;
     List<String> myTimes = new ArrayList<>();
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -34,7 +46,13 @@ public class SetInstructorTimesFragment extends Fragment{
         view = inflater.inflate(R.layout.set_up_times_fragment, container, false);
         timePicker = view.findViewById(R.id.instructor_time_picker);
         addTime = view.findViewById(R.id.pick_time_button);
-
+        submitTimes = view.findViewById(R.id.submit_times_button);
+        daySpinner = view.findViewById(R.id.day_type_spinner);
+        auth = FirebaseAuth.getInstance();
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.day_type_array, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        daySpinner.setAdapter(spinnerAdapter);
+        daySpinner.setOnItemSelectedListener(this);
 
         timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
             @SuppressLint("SetTextI18n")
@@ -52,6 +70,49 @@ public class SetInstructorTimesFragment extends Fragment{
                 Toast.makeText(getContext(), "Time chosen  " + lastTimeChosen, Toast.LENGTH_SHORT).show();
             }
         });
+
+        submitTimes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setInstructorTimes(myTimes);
+                myTimes.clear();
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        dayType = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void setInstructorTimes(List<String> times){
+        times = myTimes;
+        String instructorUUid = auth.getUid();
+
+        databaseTimesReference = FirebaseDatabase.getInstance().getReference().child("Instructors").child(instructorUUid).child("Times");
+        TimeSlot times_obj = new TimeSlot(times);
+
+        databaseTimesReference.child(dayType).setValue(times_obj)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getContext(), "Times stored", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getContext(), "Times could not be saved", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void checkTimesExists(){
+
     }
 }
